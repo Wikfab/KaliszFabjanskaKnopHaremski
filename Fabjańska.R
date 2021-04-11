0ee78b381d41650beff53fe9725a2039edf27535
Wiktoria Fabjańska <wikfab@st.amu.edu.pl>
2021-04-11 19:50:16 +0200

    Zadania

---
+++ Fabjańska.R
@@ -0,0 +1,19 @@
+#1
+
+tekst1= "TERYT 18; podkarpackie; Rzeszów; 0.2 He; A"
+tekast2="TERYT 22; pomorskie;   Gdańsk; 12 C ; B"
+str_sub(tekst1, start = 38, end = -4)
+str_sub(tekast2, start = -5, end = -4)
+
+#2
+horoskop=c("imie", "miesiac")
+tekst["imie"]=Ola
+liczba["miesiac"]=Maj
+
+#4
+tekst3=c("2019-03-11", "23.5", "19/03/12", "12.7", "2019.03.13", "11.1", "2019-marzec-14", "14.3")
+tekst3
+tekst3[c(1,3,5,7)]
+#5
+wartosci=tekst3[c(2,4,6,8)]
+wartosci
